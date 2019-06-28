2b6a2fca68ae71d5afe1e33c10e1d6a5c1e5cba3
Angelica Castillejos <angcast@mit.edu>
2019-06-28 14:18:35 -0700

    Before youtube user story

--- app/src/main/java/com/fb/flicks/MovieAdapter.java
+++ app/src/main/java/com/fb/flicks/MovieAdapter.java
@@ -1,6 +1,8 @@
 package com.fb.flicks;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +14,8 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.fb.flicks.models.Config;
 import com.fb.flicks.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -56,15 +60,30 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         holder.tvTitle.setText(movie.getTitle());
         holder.tvOverview.setText(movie.getOverview());
 
+        // determine the current orientation
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
         // build url for poster image
-        String imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        String imageUrl = null;
+
+        // if in portrait mode, load the poster image
+        if (isPortrait){
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else{
+            imageUrl = config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+        }
+
+        // get the correct placeholder and imageview for the current orientation
+        int placeholderId = isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? holder.ivPosterImage : holder.ivBackdropImage;
 
         // load image using glide
         Glide.with(context)
                 .load(imageUrl)
                 .bitmapTransform(new RoundedCornersTransformation(context, 25, 0))
-                .placeholder(R.drawable.flicks_movie_placeholder)
-                .into(holder.ivPosterImage);
+                .placeholder(placeholderId)
+                .error(placeholderId)
+                .into(imageView);
 
     }
 
@@ -75,10 +94,11 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     // create the viewholder as a static inner class
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         //track view objects
         ImageView ivPosterImage;
+        ImageView ivBackdropImage;
         TextView tvTitle;
         TextView tvOverview;
 
@@ -86,8 +106,29 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             super(itemView);
             // lookup view objects by id
             ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = (ImageView)  itemView.findViewById(R.id.ivBackdropImage);
             tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            // add this as the itemView's OnClickListener
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid
+            if(position != RecyclerView.NO_POSITION){
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
         }
     }
 }
--- app/src/main/java/com/fb/flicks/models/Movie.java
+++ app/src/main/java/com/fb/flicks/models/Movie.java
@@ -2,13 +2,22 @@ package com.fb.flicks.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
+
+@Parcel
 
 public class Movie {
-    // instance fields
-    private String title;
-    private String overview;
-    private String posterPath; // only the Path
-    private String backdropPath;
+
+
+    // instance fields : must be public for parceler
+    String title;
+    String overview;
+    String posterPath; // only the Path
+    String backdropPath;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie(){}
 
     // initialize from JSON data
     public Movie(JSONObject object) throws JSONException {
@@ -16,20 +25,31 @@ public class Movie {
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
+        voteAverage = object.getDouble("vote_average");
     }
 
     public String getTitle() {
         return title;
     }
 
+
     public String getOverview() {
         return overview;
     }
 
+
     public String getPosterPath() {
         return posterPath;
     }
+
+
     public String getBackdropPath() {
         return backdropPath;
     }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+
 }
